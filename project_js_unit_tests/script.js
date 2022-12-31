852d50e0793b5aea41754540c6909fca1f61e4eb
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2022-12-31 13:39:09 -0300

    Adicionando segundo quisito do projeto

--- project_js_unit_tests/script.js
+++ project_js_unit_tests/script.js
@@ -9,5 +9,29 @@ const average = (array) => {
     return Math.round(valores / array.length);
 };
 
-module.exports = average
+const numbers = (arr) =>{
+
+    let tpNumber = [];
+    let tpNotNumber = [];
+    let resposta;
+
+    for( let index = 0; index < arr.length; index +=1){
+        if( typeof arr[index] !== 'number'){
+            tpNotNumber.push(arr[index]);
+        }else if( typeof arr[index] === 'number'){
+            tpNumber.push(arr[index]);
+        }
+    }
+
+    if(tpNotNumber == ""){
+        resposta = true
+    }else{
+        resposta = false
+    }
+
+return resposta
+
+}
+
+module.exports = average, numbers
 
